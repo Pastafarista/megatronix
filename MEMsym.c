4d533b8991bae55b4351fb47b1bdb4d4e774dfbc
Antonio Cabrera Landín <antonio.cabrera@live.u-tad.com>
2022-11-28 18:14:27 +0100

    Cambios minimos

--- MEMsym.c
+++ MEMsym.c
@@ -16,20 +16,20 @@ int main(int argc, char** argv){
     int globaltime = 0;
     int numfallos = 0;
     unsigned char Simul_RAM[TAM_RAM];
+    FILE *fd;
 
-
-    T_CACHE_LINE linea_cache;       //RESET TOTAL DE LA CACHÉ
+    T_CACHE_LINE linea_cache;       //Reset total de la caché
     linea_cache.ETQ = 255;          // xFF -> 255
     for(int i = 0; i < TAM_LINEA; i++){
         linea_cache.Data[i] = 35;   // x23 -> 35
     }
 
-    FILE *fd;
     fd = fopen("CONTENTS_RAM", "r");
     if(fd == NULL){
-        printf("[ERROR AL CARGAR EN RAM]");
+        printf("[Error al cargar la RAM]\n");
+        return -1;
     }else{
-
+        return 0;
     }
 }
 
