eb840c2d8b372c6eda268cf4a45d35bfaf25d918
unknown <cj124357689@gmail.com>
2022-11-28 18:04:29 +0100

    Añadido la lectura del fichero RAM y el tamaño

--- MEMsym.c
+++ MEMsym.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #define TAM_LINEA 16
+#define TAM_RAM 4096
 
 typedef struct {
 unsigned char ETQ;
@@ -14,13 +15,21 @@ unsigned char Data[TAM_LINEA];
 int main(int argc, char** argv){
     int globaltime = 0;
     int numfallos = 0;
+    unsigned char Simul_RAM[TAM_RAM];
 
-    T_CACHE_LINE linea_cache;
-    linea_cache.ETQ = 255;
+
+    T_CACHE_LINE linea_cache;       //RESET TOTAL DE LA CACHÉ
+    linea_cache.ETQ = 255;          // xFF -> 255
     for(int i = 0; i < TAM_LINEA; i++){
-        linea_cache.Data[i] = 35;
+        linea_cache.Data[i] = 35;   // x23 -> 35
     }
 
+    FILE *fd;
+    fd = fopen("CONTENTS_RAM", "r");
+    if(fd == NULL){
+        printf("[ERROR AL CARGAR EN RAM]");
+    }else{
 
+    }
 }
 
