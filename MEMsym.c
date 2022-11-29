b916df133a98b485f393a6cd17de1f99ea07ee45
Antonio Cabrera Landín <antonio.cabrera@live.u-tad.com>
2022-11-29 18:36:44 +0100

    Añadido un método que obtiene la etiqueta de una dirección en decimal

--- MEMsym.c
+++ MEMsym.c
@@ -6,15 +6,18 @@
 
 #define TAM_LINEA 16
 #define TAM_RAM 4096
+#define ETIQUETA 5
+#define LINEA 3
+#define PALABRA 4
 
 typedef struct {
 unsigned char ETQ;
 unsigned char Data[TAM_LINEA];
 } T_CACHE_LINE;
 
-
 int hexToDec(char hex[3]);  //Transforma un número de hexadecimal a decimal
-int nextDir(FILE *fd);      //Obtiene la siguiente línea de accesos_memoria.txt
+int nextDir(FILE *fd);  //Obtiene la siguiente línea de accesos_memoria.txt
+int getEtq(int num);    //Obtiene la etiqueta de una dirección en decimal
 
 int main(int argc, char** argv){
     int globaltime = 0;
@@ -22,10 +25,9 @@ int main(int argc, char** argv){
     unsigned char Simul_RAM[TAM_RAM];
     unsigned int accesos_memoria[TAM_LINEA];
     FILE *fd;
-
     T_CACHE_LINE linea_cache;       
-    linea_cache.ETQ = 0xFF;  
-    printf("linea_cache etiqueta: %i\n", linea_cache.ETQ);        
+
+    linea_cache.ETQ = 0xFF;         
     for(int i = 0; i < TAM_LINEA; i++){
         linea_cache.Data[i] = 0x23;   
     }
@@ -36,7 +38,7 @@ int main(int argc, char** argv){
         return -1;
     }
     int i = 0;
-    char c = fgetc(fd);         //Leemos el contenido de la ram y lo volcamos en Simul_RAM
+    char c = fgetc(fd); //Leemos el contenido de la ram y lo volcamos en Simul_RAM
     while(c != EOF){   
         Simul_RAM[i++] = c;
         c = fgetc(fd);
@@ -48,8 +50,14 @@ int main(int argc, char** argv){
         printf("[Error al leer el acceso a memoria]\n");
         return -1;
     }
-    nextDir(fd);
-    nextDir(fd);
+    
+    int num_linea = 0;
+
+    int dir = nextDir(fd);
+    int etq = getEtq(dir);
+    printf("Etiqueda de %i: %i\n",dir, etq);
+    num_linea++;
+
     return 0;
 }
 
@@ -84,4 +92,12 @@ int nextDir(FILE *fd){
         return 0;
     }
     return hexToDec(linea);
+}
+
+int getEtq(int num){
+    int ret;
+    int mascara = ((TAM_RAM - 1) >> ETIQUETA) ^ (TAM_RAM - 1);  //111110000000 en el caso de etiqueta = 5
+    ret = num & mascara;    //Cogemos solo los 5 bits de mayor peso
+    ret = ret >> ETIQUETA;  //Los volvemos a desplazar para obtener la etiqueta
+    return ret;
 }
