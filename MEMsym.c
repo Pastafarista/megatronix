701148a3355fc326b732aeb35ae78a57b77fb011
Antonio Cabrera Landín <antonio.cabrera@live.u-tad.com>
2022-11-29 15:56:56 +0100

    Añadido método de conversión a decimal y la lectura de accesos_memoria.txt

--- MEMsym.c
+++ MEMsym.c
@@ -1,8 +1,9 @@
 //Antonio Cabrera y Alejandro Gómez
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <unistd.h>
+
 #define TAM_LINEA 16
 #define TAM_RAM 4096
 
@@ -12,6 +13,8 @@ unsigned char Data[TAM_LINEA];
 } T_CACHE_LINE;
 
 
+int hexToDec(char hex[3]);  //Transforma un número hexadecimal representado como un array de char de tamaño 3 a un entero en base decimal
+
 int main(int argc, char** argv){
     int globaltime = 0;
     int numfallos = 0;
@@ -19,10 +22,10 @@ int main(int argc, char** argv){
     unsigned int accesos_memoria[TAM_LINEA];
     FILE *fd;
 
-    T_CACHE_LINE linea_cache;       //Reset total de la caché
-    linea_cache.ETQ = 255;          // xFF -> 255
+    T_CACHE_LINE linea_cache;       
+    linea_cache.ETQ = 0xFF;          
     for(int i = 0; i < TAM_LINEA; i++){
-        linea_cache.Data[i] = 35;   // x23 -> 35
+        linea_cache.Data[i] = 0x23;   
     }
 
     fd = fopen("CONTENTS_RAM.bin", "r");
@@ -43,10 +46,36 @@ int main(int argc, char** argv){
             printf("[Error al leer el acceso a memoria]\n");
             return -1;
         }else{
-            int direccion;
-            
+            char linea[3];
+            if(fgets( linea, 4, fd ) != NULL){
+                printf("%i\n", hexToDec(linea));
+            }
             return 0;
         }
     }
 }
 
+int hexToDec(char hex[3]){
+    int len = strlen(hex) - 1;
+    int decimal, val;
+
+    for(int i = 0; hex[i] != '\0'; i++)
+    {
+        if(hex[i] >= '0' && hex[i] <= '9')
+        {
+            val = hex[i] - 48;
+        }
+        else if(hex[i] >= 'a' && hex[i] <= 'f')
+        {
+            val = hex[i] - 97 + 10;
+        }
+        else if(hex[i] >= 'A' && hex[i] <= 'F')
+        {
+            val = hex[i] - 65 + 10;
+        }
+
+        decimal += val * pow(16, len);
+        len--;
+    }
+    return decimal;
+}
